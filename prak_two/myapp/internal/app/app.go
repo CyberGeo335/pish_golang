cd72f3430736027498580536c4f7ba1ee05ab207
CyberGeo335 <george.dev@mail.ru>
2025-10-09 15:20:55 +0300

    ex A

--- prak_two/myapp/internal/app/app.go
+++ prak_two/myapp/internal/app/app.go
@@ -13,6 +13,17 @@ type pingResp struct {
 	Time   string `json:"time"`
 }
 
+func withRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.Header.Get("X-Request-Id")
+		if id == "" {
+			id = utils.NewID16()
+		}
+		w.Header().Set("X-Request-Id", id)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func Run() {
 	mux := http.NewServeMux()
 
@@ -37,4 +48,12 @@ func Run() {
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		utils.LogError("server error: " + err.Error())
 	}
+
+	handler := withRequestID(mux)
+
+	utils.LogInfo("Server is starting on :8080")
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		utils.LogError("server error: " + err.Error())
+	}
+
 }
